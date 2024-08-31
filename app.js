1c48b8937ae7b155864f296ead1ac03d9d32f230
vishnug1110 <77599885+vishnug1110@users.noreply.github.com>
2024-08-31 22:24:52 +0530

    initialized

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose=require('mongoose');
+const app = express()
+const port = 3000
+const path=require('path');
+const Campground=require('./models/campground');
+const { compile } = require('ejs');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+
+const db=mongoose.connection
+
+db.on("error",console.error.bind(console,"connection error:"))
+db.once("open",()=>{
+    console.log("database connected")
+})
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/makeCampground', async(req, res) => {
+    const camp=new Campground({title:'my backyard',description:'cheap camping'});
+    await camp.save()
+    res.send(camp)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
