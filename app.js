dbc373c2f6d00f0675ad9ad1fa20d3c2ed2cd832
vishnug1110 <77599885+vishnug1110@users.noreply.github.com>
2024-09-02 21:43:33 +0530

    made a show page strcture for a campground

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index',{campgrounds})
   })
 
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground= await Campground.findById(req.params.id)
+    res.render('campgrounds/show',{campground})
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
