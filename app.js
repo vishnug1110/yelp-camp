0e7536fb87dd56a70256b88965b4cabdebda3b5e
vishnug1110 <77599885+vishnug1110@users.noreply.github.com>
2024-09-02 23:03:22 +0530

    Post request for adding new created campground in the db

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+app.use(express.urlencoded({ extended: true }));       //this is for converting the req data sent in meaningful form
+
 const db=mongoose.connection
 
 db.on("error",console.error.bind(console,"connection error:"))
@@ -30,10 +32,22 @@ app.get('/campgrounds', async(req, res) => {
     res.render('campgrounds/index',{campgrounds})
   })
 
+app.get('/campgrounds/new', async(req, res) => {
+    res.render('campgrounds/new')
+  })
+
+  app.post('/campgrounds', async(req, res) => {
+    const campground=new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
+  })
+
 app.get('/campgrounds/:id', async(req, res) => {
     const campground= await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground})
   })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
